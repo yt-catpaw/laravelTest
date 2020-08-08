396feca6d33910af3b6d902e4c669c931f8b6d2c
yamanishiyusuke <nextcentury2000@gmail.com>
2020-08-08 17:16:46 +0900

    fix migration

--- database/migrations/2020_08_07_092919_create_users_table.php
+++ database/migrations/2020_08_07_092919_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMtUserTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,14 +14,14 @@ class CreateMtUserTable extends Migration
     public function up()
     {
         Schema::create('mt_users', function (Blueprint $table) {
-            $table->integer('id', 11)->autoIncrement();
+            $table->increments('id');
             $table->string('name', 32);
             $table->string('password', 255);
             $table->string('email', 128);
             $table->string('postal_code', 8);
             $table->string('address', 128);
             $table->string('phone', 15);
-            $table->tinyInteger('active_flg', 1);
+            $table->tinyInteger('active_flg');
             $table->timestamps();
         });
     }
